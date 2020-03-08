6024e04e2ed400f7d0d9799be375556bedb4f940
Igor Razvodovsky <igor.razvodovsky@wellplayed.lv>
2020-03-08 11:43:48 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,234 @@
+<template>
+  <div class="paralax">
+    <div class="grid">
+      <!--FILLING-->
+      <!--1-->
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <!--2-->
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__item__c-s-2 p-0">
+        <!--<img class="d-block" src="../images/responsive2.svg"/>-->
+      </span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__c-s-2"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer">
+        <!-- <img class="grid__item__image swirl-1" src="../images/swirl-1.png"/> -->
+      </span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <!--3-->
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__item__c-s-2"></span>
+      <span class="grid__item grid__spacer"></span>
+      <!--4-->
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__item__c-s-2"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__spacer"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2 p-0">
+        <!-- ${require("!raw-loader!../images/border-01.svg")} -->
+      </span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <!--5-->
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <!--6-->
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m p-0">
+        <!--<img class="d-block" src="../images/responsive.svg"/>-->
+      </span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <!--7-->
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <!--8-->
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m">
+        <!-- <img class="grid__item__image swirl-3" src="../images/swirl-3.png"/> -->
+      </span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <!--9-->
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"> </span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <!--10-->
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"> </span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <!--11-->
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <!--12-->
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m grid__item__c-s-2"></span>
+      <span class="grid__item grid__item__m"></span>
+      <span class="grid__item grid__item__m"></span>
+
+      <div class="grid__item header header__name">
+        <div><span>Igor Razvodovsky</span>, User Experience Designer</div>
+      </div>
+      <ul class="grid__item header header__links">
+        <li>
+          <a href="https://www.linkedin.com/in/igor-razvodovsky-231909/"
+            >LinkedIn</a
+          >
+        </li>
+        <li><a href="https://medium.com/@igorrazvodovsky">Medium</a></li>
+      </ul>
+      <ul class="grid__item header header__contacts">
+        <li><a href="mailto:igorrazvodovsky@gmail.com">Email</a></li>
+        <li><a href="tel:+37129828442">Mobile</a></li>
+      </ul>
+      <!--<h1 id="word" class="">I’m Igor,<br/>a product</br>designer.</h1>-->
+      <div class="grid__item heading">
+        <h1>
+          <span id="word"
+            >Ther<span id="apostrophe">'</span>re many designers and this is one
+            of them</span
+          >
+        </h1>
+      </div>
+
+      <div class="grid__item about">
+        <p>
+          Friendly designer creating clear, robust, consistent, elegant systems
+          that avoid redundancy. Currently working at
+          <a href="https://home.kuehne-nagel.com/">Kuehne + Nagel</a>.
+        </p>
+        <div class="about__expanded">
+          <p>
+            Most recently I have been working on back-office&nbsp;CRM system for
+            educational start-up.
+          </p>
+          <p>
+            Previously I made social games at Superium, and prior to that, I
+            worked as a product designer at the
+            <a href="https://ok.ru/">Odnoklassniki</a> social network &
+            <a href="http://dego.lv/">Dego</a> design agency.
+          </p>
+        </div>
+      </div>
+
+      <h2 class="grid__item spotify">
+        <span class="subheader">Writing about</span>
+        Turning Spotify into Jukebox
+      </h2>
+      <p class="grid__item spotify">
+        A concept investigating the possibility of playing music in public
+        places using streaming services.
+      </p>
+      <div class="grid__item spotify links">
+        <a
+          class=""
+          href="https://uxdesign.cc/turning-spotify-into-jukebox-c6f0ae031284"
+          >Read on Medium &rarr;</a
+        >
+      </div>
+      <div class="grid__item spotify img my-3">
+        <!-- ${require("!raw-loader!../images/jukebox__img.svg")}
+            ${require("!raw-loader!../images/jukebox__img.svg")} -->
+      </div>
+
+      <h2 id="scene1" class="grid__item dal">
+        <span class="subheader">Experimenting with</span>
+        medium for a&nbsp;recipe
+        <div class="dal__veg">
+          <!-- ${require("!raw-loader!../images/dal__onion.svg")}
+                ${require("!raw-loader!../images/dal__garlic.svg")}
+                ${require("!raw-loader!../images/dal__chili.svg")}
+                ${require("!raw-loader!../images/dal__knife.svg")}
+                ${require("!raw-loader!../images/dal__ginger.svg")}
+                ${require("!raw-loader!../images/dal__pieces.svg")} -->
+        </div>
+      </h2>
+      <p class="grid__item dal">
+        A side-project with the goal to create a product that does one job
+        incredibly well and learn a couple of new things along the way.
+      </p>
+      <div class="grid__item links dal">
+        <a
+          href="https://medium.com/@igorrazvodovsky/searching-for-a-medium-for-a-recipe-c707ccfab23a"
+          >Case Study &rarr;</a
+        >
+        <a href="https://assistant.google.com/services/a/uid/0000009f815d8acf"
+          >Google Assistant &rarr;</a
+        >
+        <a
+          href="https://play.google.com/store/apps/details?id=com.irazvodovsky.dal"
+          >Google Play &rarr;</a
+        >
+      </div>
+
+      <div class="grid__item img dal">
+        <div class="dal__phone">
+          <!-- ${require("!raw-loader!../images/dal__phone.svg")} -->
+          <!-- <img class="dal__phone__screen" src="../images/dal__screen.png"/> -->
+        </div>
+        <div class="dal__speaker">
+          <!-- <img class="dal__speaker__speaker" src="../images/dal__speaker.png"/>
+                <img class="dal__speaker__sound" src="../images/dal__speaker__sounds.gif"/>
+                <img class="dal__speaker__shadow" src="../images/dal__speaker__shadow.png"/> -->
+        </div>
+      </div>
+
+      <!-- Illustration -->
+      <span class="grid__item black">
+        <!-- <img class="grid__item__image abstract abstract__y" src="../images/abstract_y.png"/> -->
+        <!-- <img class="grid__item__image abstract abstract__r" src="../images/abstract_r.png"/> -->
+      </span>
+    </div>
+  </div>
+</template>
