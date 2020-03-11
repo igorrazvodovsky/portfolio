244658773fa445b294abd9ecc777fcdaf198b66a
Igors Razvodovskis <igors.razvodovskis@Kuehne-Nagel.com>
2020-03-11 17:49:41 +0200

    Invert styles in project view

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="container" :class="{ case: page !== 'index' }">
     <header>
-      <div class="title">
-        <nuxt-link to="/">Igor Razvodovsky</nuxt-link
-        ><span v-if="page == 'index'">, User Experience&nbsp;Designer</span>
+      <div class="title text--secondary">
+        <nuxt-link to="/">Igor Razvodovsky</nuxt-link>
+        <span v-if="page == 'index'">, User Experience&nbsp;Designer</span>
       </div>
-      <div v-if="page !== 'index'" class="subtitle text--secondary">
-        Work
-      </div>
+      <div v-if="page !== 'index'" class="subtitle text--secondary">Work</div>
       <!-- <a href>Email</a> -->
     </header>
     <transition name="fade">
